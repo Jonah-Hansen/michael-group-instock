25e7c7e1a9e813f15ff0a8f4617cd1782ee6c7c1
Jonah-Hansen <tce_mc@telus.net>
2022-11-21 20:26:56 -0600

    pull label with input into its own component

---
+++ src/components/TextInput/TextInput.js
@@ -0,0 +1,11 @@
+
+function TextInput() {
+  return (
+    <label>
+      <h3>Warehouse Name</h3>
+      <input type="text" placeholder='Warehouse Name' />
+    </label>
+  )
+}
+
+export default TextInput
