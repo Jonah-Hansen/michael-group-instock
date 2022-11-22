9daa3948f71e8d6b762f81db3f0f87ba60ed4fd1
Jonah-Hansen <tce_mc@telus.net>
2022-11-22 16:59:59 -0600

    add page header component

--- src/pages/NewWarehouse/NewWarehouse.js
+++ src/pages/NewWarehouse/NewWarehouse.js
@@ -1,16 +1,12 @@
-import { useNavigate } from 'react-router-dom'
+import PageHeader from '../../components/PageHeader/PageHeader'
 import WarehouseForm from '../../components/WarehouseForm/WarehouseForm'
-import './NewWarehouse.scss'
+
 function NewWarehouse() {
-  const navigate = useNavigate()
   return (
-    <main className='new-warehouse'>
-      <header className='new-warehouse__header'>
-        <button className='new-warehouse__back' onClick={() => navigate(-1)} />
-        <h1 className='new-warehouse__heading'>Add New Warehouse</h1>
-      </header>
+    <>
+      <PageHeader text='Add New Warehouse' />
       <WarehouseForm />
-    </main>
+    </>
   )
 }
 
