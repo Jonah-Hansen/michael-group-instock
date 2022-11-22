749321daf7bec8f2a2fd1e59a2d43b53c0dc28fc
Jonah-Hansen <tce_mc@telus.net>
2022-11-21 20:23:12 -0600

    pull form into its own component

--- src/pages/NewWarehouse/NewWarehouse.js
+++ src/pages/NewWarehouse/NewWarehouse.js
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom'
+import WarehouseForm from '../../components/WarehouseForm/WarehouseForm'
 import './NewWarehouse.scss'
 function NewWarehouse() {
   const navigate = useNavigate()
@@ -8,54 +9,7 @@ function NewWarehouse() {
         <button className='new-warehouse__back' onClick={() => navigate(-1)} />
         <h1 className='new-warehouse__heading'>Add New Warehouse</h1>
       </header>
-
-      <form className='warehouse-form'>
-        <div className='warehouse-form__details'>
-          <section className='warehouse-form__details-warehouse'>
-            <h2>Warehouse Details</h2>
-            <label>
-              <h3>Warehouse Name</h3>
-              <input type="text" placeholder='Warehouse Name' />
-            </label>
-            <label>
-              <h3>Street Address</h3>
-              <input type="text" placeholder='Street Address' />
-            </label>
-            <label>
-              <h3>City</h3>
-              <input type="text" placeholder='City' />
-            </label>
-            <label>
-              <h3>Country</h3>
-              <input type="text" placeholder='Country' />
-            </label>
-          </section>
-          <section className='warehouse-form__details-contact'>
-            <h2>Contact Details</h2>
-            <label>
-              <h3>Contact Name</h3>
-              <input type="text" placeholder='Contact Name' />
-            </label>
-            <label>
-              <h3>Position</h3>
-              <input type="text" placeholder='Position' />
-            </label>
-            <label>
-              <h3>Phone Number</h3>
-              <input type="text" placeholder='Phone Number' />
-            </label>
-            <label>
-              <h3>Email</h3>
-              <input type="text" placeholder='Email' />
-            </label>
-          </section>
-        </div>
-        <div className='warehouse-form__buttons'>
-          <button className='button--cancel'>Cancel</button>
-          <button className='button--confirm'>+ Add Warehouse</button>
-        </div>
-      </form>
-
+      <WarehouseForm />
     </main>
   )
 }
