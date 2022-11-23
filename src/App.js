8a17f8291ed67a231eb3892bf870e3a2facdd690
MarceloVBrasil <marcelo.vital.brasil@gmail.com>
2022-11-22 23:07:15 -0500

    create new inventory item form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import './App.scss';
+import AddNewInventoryItem from "./pages/AddNewInventoryItem/AddNewInventoryItem";
 import NewWarehouse from "./pages/NewWarehouse/NewWarehouse";
 
 function App() {
@@ -18,7 +19,7 @@ function App() {
         <Route path="/inventory" />
         <Route path="/inventory/:inventoryid" />
         <Route path="/inventory/:inventoryid/edit" />
-        <Route path="/inventory/new" />
+        <Route path="/inventory/new" element={ <AddNewInventoryItem />} />
       </Routes>
     </BrowserRouter>
   );
