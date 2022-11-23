cc7f6d1d75b19caeb22cd8ab459ce74a5ea965e0
Lucas Rodrigues <lucas.rodrigues@iqbridge.ca>
2022-11-23 12:23:29 -0500

    fix path to styles, add link to logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,15 @@
 import './Header.scss';
 import InStockLogo from "../../assets/logo/InStock-Logo.svg";
-import {NavLink} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 
 function Header() {
     return (
       <header className="header">
         <div className="header__logo">
-        <img 
+        <Link to={"/"}><img 
         src={InStockLogo}
         alt="in-stock-logo" 
-        />
+        /></Link>
         </div>
         <nav className="nav">
           <NavLink to={"/warehouses"} className={"nav__link"}>Warehouses</NavLink>
