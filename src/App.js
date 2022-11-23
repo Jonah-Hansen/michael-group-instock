c168e4620698fcfd0eb8c817538f41fbe393f0c3
Jonah-Hansen <tce_mc@telus.net>
2022-11-23 12:49:18 -0600

    wrap routes in main and apply styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import './App.scss';
+import Header from "./components/Header/Header";
 import NewWarehouse from "./pages/NewWarehouse/NewWarehouse";
-import Header from "./components/Header/Header"
 
 function App() {
   return (
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          
-          <Route className="" path="/" element={<Navigate to='/warehouses' />} />
-
+    <BrowserRouter>
+      <Header />
+      <main className="page-container" >
+        <Routes >
+          <Route path="/" element={<Navigate to='/warehouses' />} />
           {/* Warehouse Routes */}
           <Route path="/warehouses" />
           <Route path="/warehouses/:warehouseid" />
           <Route path="/warehouses/:warehouseid/edit" />
           <Route path="/warehouses/new" element={<NewWarehouse />} />
 
-        {/* Inventory Routes */}
-        <Route path="/inventory" />
-        <Route path="/inventory/:inventoryid" />
-        <Route path="/inventory/:inventoryid/edit" />
-        <Route path="/inventory/new" />
-      </Routes>
+          {/* Inventory Routes */}
+          <Route path="/inventory" />
+          <Route path="/inventory/:inventoryid" />
+          <Route path="/inventory/:inventoryid/edit" />
+          <Route path="/inventory/new" />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
