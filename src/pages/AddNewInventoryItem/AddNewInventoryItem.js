f5266b22e6ae22f38a97247785b018a572bd3f3c
MarceloVBrasil <marcelo.vital.brasil@gmail.com>
2022-11-23 13:37:20 -0500

    create footer component

--- src/pages/AddNewInventoryItem/AddNewInventoryItem.js
+++ src/pages/AddNewInventoryItem/AddNewInventoryItem.js
@@ -1,19 +1,17 @@
 import React from 'react'
 import PageHeader from '../../components/PageHeader/PageHeader'
-// import TextInput from '../../components/TextInput/TextInput'
-// import DropDownMenu from '../../components/DropDownMenu/DropDownMenu'
-// import RadioButton from '../../components/RadioButton/RadioButton'
-// import Button from '../../components/Button/Button'
 import './AddNewInventoryItem.scss'
 import NewInventoryItemForm from '../../components/NewInventoryItemForm/NewInventoryItemForm'
+import Footer from '../../components/Footer/Footer'
 
 export default function AddNewInventoryItem() {
   return (
       <div className='background'>
         <div className='page'>              
-              <PageHeader text={'add new inventory item'} />
-              <NewInventoryItemForm />
+            <PageHeader text={'add new inventory item'} />
+            <NewInventoryItemForm />
         </div>
+        <Footer />
     </div>
   )
 }
