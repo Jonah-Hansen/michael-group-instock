adb827508251a53a115da9de774029d5ccecc1d9
MarceloVBrasil <marcelo.vital.brasil@gmail.com>
2022-11-23 16:25:38 -0500

    create Inventory Item Detail Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import AddNewInventoryItem from "./pages/AddNewInventoryItem/AddNewInventoryItem";
 import NewWarehouse from "./pages/NewWarehouse/NewWarehouse";
 import Header from "./components/Header/Header"
+import InventoryItem from './pages/InventoryItem/InventoryItem'
 
 function App() {
   return (
@@ -20,7 +21,7 @@ function App() {
 
         {/* Inventory Routes */}
         <Route path="/inventory" />
-        <Route path="/inventory/:inventoryid" />
+        <Route path="/inventory/:inventoryid" element={<InventoryItem /> } />
         <Route path="/inventory/:inventoryid/edit" />
         <Route path="/inventory/new" element={ <AddNewInventoryItem />} />
       </Routes>
