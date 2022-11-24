8a00c2c4b15e2347d99e1bc2853cbb8bf5883c10
Jonah-Hansen <tce_mc@telus.net>
2022-11-23 18:31:19 -0600

    add field validation on submit and associated styling

--- src/components/WarehouseForm/WarehouseForm.js
+++ src/components/WarehouseForm/WarehouseForm.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { emptyValues } from '../../helpers/validation'
+import * as validate from '../../helpers/validation'
 import Button from '../Button/Button'
 import TextInput from '../TextInput/TextInput'
 import './WarehouseForm.scss'
@@ -12,10 +13,14 @@ const {
 
 function WarehouseForm() {
   const navigate = useNavigate()
+  const [error, setError] = useState({})
 
   const handleSubmit = (event) => {
     event.preventDefault()
+    //reset error
+    setError({})
     const { whName, addr, city, country, cName, position, phone, email } = event.target
+
     //build new warehouse object.
     const newWarehouse = {
       warehouse_name: whName.value,
@@ -28,29 +33,45 @@ function WarehouseForm() {
       contact_email: email.value,
     }
 
-    console.log(emptyValues(newWarehouse));
-
-    //   axios.post(`${URL}:${PORT}/warehouse`, newWarehouse)
-    //     .catch(err => console.error(err.response.data))
+    if (validate.values(newWarehouse).length !== 0 || !validate.phone(newWarehouse.contact_phone) || !validate.email(newWarehouse.contact_email)) {
+      const err = {}
+      //set error for invalid phone number
+      if (!validate.phone(newWarehouse.contact_phone)) {
+        err['contact_phone'] = 'please enter a valid 10 digit phone number'
+      }
+      //set error for invalid email
+      if (!validate.email(newWarehouse.contact_email)) {
+        err['contact_email'] = 'please enter a valid email address'
+      }
+      //set error for keys with missing values
+      validate.values(newWarehouse)
+        .forEach(key => err[key] = 'this field is required')
+      setError(err)
+      return
+    }
+    setError({})
+    event.target.reset()
+    console.log('form submitted');
+    // axios.post(`${URL}:${PORT}/warehouse`, newWarehouse)
+    //   .catch(err => console.error(err.response.data))
   }
-
 
   return (
     <form className='warehouse-form' onSubmit={handleSubmit} noValidate >
       <div className='warehouse-form__details-container'>
         <section className='warehouse-form__details--first'>
           <h2 className='warehouse-form__heading'>Warehouse Details</h2>
-          <TextInput type='small' label='Warehouse Name' name={'whName'} />
-          <TextInput type='small' label='street address' name={'addr'} />
-          <TextInput type='small' label='city' name={'city'} />
-          <TextInput type='small' label='country' name={'country'} />
+          <TextInput type='small' label='Warehouse Name' name={'whName'} error={error.warehouse_name} />
+          <TextInput type='small' label='street address' name={'addr'} error={error.address} />
+          <TextInput type='small' label='city' name={'city'} error={error.city} />
+          <TextInput type='small' label='country' name={'country'} error={error.country} />
         </section>
         <section className='warehouse-form__details'>
           <h2 className='warehouse-form__heading'>Contact Details</h2>
-          <TextInput type='small' label='contact name' name={'cName'} />
-          <TextInput type='small' label='position' name={'position'} />
-          <TextInput type='tel' label='phone' name={'phone'} />
-          <TextInput type='email' label='email' name={'email'} />
+          <TextInput type='small' label='contact name' name={'cName'} error={error.contact_name} />
+          <TextInput type='small' label='position' name={'position'} error={error.contact_position} />
+          <TextInput type='tel' label='phone' name={'phone'} error={error.contact_phone} />
+          <TextInput type='email' label='email' name={'email'} error={error.contact_email} />
         </section>
       </div>
       <div className='warehouse-form__buttons'>
--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -1,6 +1,22 @@
+import { validate } from "email-validator"
+
 //returns the list of keys with empty values.
-export const emptyValues = (formObject) => {
+export function values(formObject) {
   const keys = Object.keys(formObject)
   return keys.filter(key => formObject[key].trim().length === 0)
 }
 
+//returns phone number in format +1 (646) 123-1234
+//or false if there are missing numbers
+export function phone(string) {
+  //remove non numbers from from string
+  const s = string.split('').filter(char => char !== ' ' && !isNaN(char))
+  //return false is not enough numbers
+  if (s.length !== 10) return false
+  return `+1 (${s[0] + s[1] + s[2]}) ${s[3] + s[4] + s[5]}-${s[6] + s[7] + s[8] + s[9]}`
+}
+
+//returns true or false if email is valid 
+export function email(string) {
+  return validate(string)
+}
