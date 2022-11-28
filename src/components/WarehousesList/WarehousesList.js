f73d0f2e835af9d01dd652b173fa1d29ccbbb473
MarceloVBrasil <marcelo.vital.brasil@gmail.com>
2022-11-28 15:33:15 -0500

    implement warehouse sorting

--- src/components/WarehousesList/WarehousesList.js
+++ src/components/WarehousesList/WarehousesList.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../../helpers/axiosInstance";
 import WarehousesListItem from "../WarehousesListItem/WarehousesListItem";
 import "./WarehousesList.scss";
 
-function WarehousesList() {
+function WarehousesList({ warehouseOrdered, addressOrdered, contactNameOrdered, contactInfoOrdered }) {
 
   const [warehousesData, setWarehousesData] = useState([])
 
@@ -16,6 +16,22 @@ function WarehousesList() {
         console.log(error)
       });
   }, []);
+
+  useEffect(() => {
+    sortByWarehouse()
+  }, [warehouseOrdered])
+
+    useEffect(() => {
+    sortByAddress()
+    }, [addressOrdered])
+  
+    useEffect(() => {
+    sortByContactName()
+    }, [contactNameOrdered])
+  
+    useEffect(() => {
+    sortByContactInfo()
+  }, [contactInfoOrdered])
 
   return (
     <section className="warehouse-list">
@@ -32,6 +48,54 @@ function WarehousesList() {
       />)}
     </section>
   )
+
+  async function sortByWarehouse() {
+    let response;
+    try {
+      if (warehouseOrdered) response = await axiosInstance.get('/warehouse/sortByWarehouseASC')
+      else response = await axiosInstance.get('warehouse/sortByWarehouseDESC')
+      setWarehousesData(response.data)
+
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  async function sortByAddress() {
+    let response;
+    try {
+      if (addressOrdered) response = await axiosInstance.get('/warehouse/sortByAddressASC')
+      else response = await axiosInstance.get('warehouse/sortByAddressDESC')
+      setWarehousesData(response.data)
+
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  async function sortByContactName() {
+    let response;
+    try {
+      if (contactNameOrdered) response = await axiosInstance.get('/warehouse/sortByContactNameASC')
+      else response = await axiosInstance.get('warehouse/sortByContactNameDESC')
+      setWarehousesData(response.data)
+
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+    async function sortByContactInfo() {
+    let response;
+    try {
+      if (contactInfoOrdered) response = await axiosInstance.get('/warehouse/sortByContactInfoASC')
+      else response = await axiosInstance.get('warehouse/sortByContactInfoDESC')
+      setWarehousesData(response.data)
+
+    } catch (error) {
+      alert(error)
+    }
+  }
 }
 
 export default WarehousesList;
--- src/pages/WarehousesPage/WarehousesPage.js
+++ src/pages/WarehousesPage/WarehousesPage.js
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import PageHeader from "../../components/PageHeader/PageHeader"
 import WarehousesHeadings from "../../components/WarehousesHeadings/WarehousesHeadings";
 import WarehousesList from "../../components/WarehousesList/WarehousesList";
 
 function WarehousesPage() {
+  const [warehouseOrdered, setWarehouseOrdered] = useState(false)
+  const [addressOrdered, setAddressOrdered] = useState(false)
+  const [contactNameOrdered, setContactNameOrdered] = useState(false)
+  const [contactInfoOrdered, setContactInfoOrdered] = useState(false)
   return (
     <>
-    <PageHeader type={"list"} text={"warehouses"} />
-    <WarehousesHeadings />
-    <WarehousesList />
+      <PageHeader type={"list"} text={"warehouses"}/>
+      <WarehousesHeadings
+        setWarehouseOrdered={setWarehouseOrdered}
+        setAddressOrdered={setAddressOrdered}
+        setContactNameOrdered={setContactNameOrdered}
+        setContactInfoOrdered={setContactInfoOrdered} />
+      <WarehousesList
+        warehouseOrdered={warehouseOrdered}
+        addressOrdered={addressOrdered}
+        contactNameOrdered={contactNameOrdered}
+        contactInfoOrdered={contactInfoOrdered}
+      />
     </>
   )
 }
