fe3b669523f99e37aadef1bccebabdfeba809d22
Lucas Rodrigues <lucas.rodrigues@iqbridge.ca>
2022-11-28 13:33:40 -0500

    removed 6-9 in warehouses page

--- src/pages/WarehousesPage/WarehousesPage.js
+++ src/pages/WarehousesPage/WarehousesPage.js
@@ -2,12 +2,6 @@ import PageHeader from "../../components/PageHeader/PageHeader"
 import WarehousesHeadings from "../../components/WarehousesHeadings/WarehousesHeadings";
 import WarehousesList from "../../components/WarehousesList/WarehousesList";
 
-
-const {
-   REACT_APP_PORT: PORT = "8080",
-   REACT_APP_BACKEND_URL: URL = "http://localhost"
- } = process.env;
-
 function WarehousesPage() {
   return (
     <>
