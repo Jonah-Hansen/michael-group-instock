632e30a76dec156de7a2e1576d2fa803f92f057a
Adam Pietrangelo <apietrangelo44@gmail.com>
2022-11-28 01:25:57 -0500

    adds styling for mobile, full functionality with axios calls

--- src/components/WarehouseInventoryListItem/WarehouseInventoryListItem.js
+++ src/components/WarehouseInventoryListItem/WarehouseInventoryListItem.js
@@ -8,14 +8,24 @@ function WarehouseInventoryListItem ({name , quantity , category , status}) {
     return (
         <div className="inv-row">
             <div className="inv-row__inv">
-                <p className="inv-row__inv-item">{name}</p>
-                <img className="inv-row__inv-icon" src={arrowIcon} alt="chevron right" />
+                <p className="inv-row__inv-heading--mobile">INVENTORY ITEM</p>
+                <div className="inv-row__inv-item-con">
+                    <p className="inv-row__inv-item">{name}</p>
+                    <img className="inv-row__inv-icon" src={arrowIcon} alt="chevron right" /> 
+                </div>
             </div>
-            <p className="inv-row__cat">{category}</p>
+            <div className="inv-row__cat-mobile">
+                <p className="inv-row__inv-heading--mobile">CATEGORY</p>
+                <p className="inv-row__cat">{category}</p>
+            </div>
             <div className="inv-row__stock">
+                <p className="inv-row__inv-heading--mobile">STATUS</p>
                 <p className={quantity ? 'inv-row__in-stock' : 'inv-row__out-stock' }>{status}</p>
             </div>
-            <p className="inv-row__quantity">{quantity}</p>
+            <div className="inv-row__quantity-mobile">
+                <p className="inv-row__inv-heading--mobile">QTY</p>
+                <p className="inv-row__quantity">{quantity}</p>
+            </div>
             <div className="inv-row__actions">
                 <img className="inv-row__actions-delete" src={deleteIcon} alt="red garbage can delete" />
                 <img className="inv-row__actions-edit" src={editIcon} alt="blue pen representing edit" />
